c2dec7811a701155287fd6675ba0da071c94ef77
Hans Steffens <hans@steffens.co>
2024-01-28 20:10:49 -0500

    Set up Header Nav with auth

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 const Nav = () => {
+  const { authUser } = useContext(UserContext);
   return (
     <nav>
-        <ul className="header--signedout">
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/signin">Sign In</Link></li>
-        </ul>
+      <ul className="header--signedout">
+        {authUser === null ? (
+          <>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <span>Welcome {authUser.firstName}</span>
+            <li>
+              <Link to="/signout">Sign Out</Link>
+            </li>
+          </>
+        )}
+      </ul>
     </nav>
   );
-}
+};
 
 export default Nav;
