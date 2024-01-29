416008a3b070b87fa32646799e464ac16829a767
Hans Steffens <hans@steffens.co>
2024-01-29 11:09:29 -0500

    Setup CreateCourse and PrivateRoute

---
+++ client/src/components/CreateCourse.js
@@ -0,0 +1,114 @@
+import { useContext, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../utils/apiHelper";
+
+import ErrorsDisplay from "./ErrorsDisplay";
+import UserContext from "../context/UserContext";
+
+const CreateCourse = () => {
+  const { authUser } = useContext(UserContext);
+  // Navigating Course Routes Programmatically.
+  const navigate = useNavigate();
+
+  // State
+  const title = useRef(null);
+  const description = useRef(null);
+  const estimatedTime = useRef(null);
+  const materialsNeeded = useRef(null);
+  const [errors, setErrors] = useState([]);
+
+  // Event handlers
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const course = {
+      userId: authUser.id,
+      title: title.current.value,
+      description: description.current.value,
+      estimatedTime: estimatedTime.current.value,
+      materialsNeeded: materialsNeeded.current.value,
+    };
+
+    // POST for creating a new course. 
+    try {
+      const response = await api("/courses", "POST", course, authUser);
+      if (response.status === 201) {
+        // Changing the path in response to the new course detailed page.
+        const path = response.headers.get("Location");
+        navigate(path);
+      } else if (response.status === 400) {
+        const jsonData = await response.json();
+        setErrors(jsonData.errors);
+      } else if (response.status === 500) {
+        navigate(`/error`);
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.log(error);
+      navigate("/error");
+    }
+  };
+
+  const handleCancel = (event) => {
+    event.preventDefault();
+    navigate("/");
+  };
+
+  return (
+    <div className="wrap">
+      <h2>Create Course</h2>
+      <ErrorsDisplay errors={errors} />
+      <form onSubmit={handleSubmit}>
+        <div className="main--flex">
+          <div>
+            <label htmlFor="courseTitle">Course Title</label>
+            <input
+              id="courseTitle"
+              name="courseTitle"
+              type="text"
+              ref={title}
+              defaultValue=""
+            />
+
+            <p>
+              By {authUser.firstName} {authUser.lastName}
+            </p>
+
+            <label htmlFor="courseDescription">Course Description</label>
+            <textarea
+              id="courseDescription"
+              name="courseDescription"
+              ref={description}
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="estimatedTime">Estimated Time</label>
+            <input
+              id="estimatedTime"
+              name="estimatedTime"
+              type="text"
+              ref={estimatedTime}
+              defaultValue=""
+            />
+
+            <label htmlFor="materialsNeeded">Materials Needed</label>
+            <textarea
+              id="materialsNeeded"
+              name="materialsNeeded"
+              ref={materialsNeeded}
+            ></textarea>
+          </div>
+        </div>
+        <button className="button" type="submit">
+          Create Course
+        </button>
+        <button className="button button-secondary" onClick={handleCancel}>
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateCourse;
